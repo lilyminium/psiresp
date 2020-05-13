4aef7b5fe0c3c7709f9a26210c99c2435e59a054
Lily <lily.wang@anu.edu.au>
2020-05-14 09:54:40 +1000

    added datafile

--- tests/test_multiresp.py
+++ tests/test_multiresp.py
@@ -9,7 +9,7 @@ import sys
 import numpy as np
 
 from numpy.testing import assert_almost_equal, assert_allclose
-from .utils import mol_from_file, charges_from_red_file
+from .utils import mol_from_file, charges_from_red_file, datafile
 
 
 @pytest.mark.parametrize('stage_2,a,redname', [
