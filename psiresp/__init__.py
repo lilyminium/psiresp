0bb1c5d8ebd148b7b65f3f874aa4dc73dd1f94d3
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2021-11-08 20:45:25 -0800

    Update config (#38)

--- psiresp/__init__.py
+++ psiresp/__init__.py
@@ -1,3 +1,8 @@
+"""
+psiresp
+A RESP plugin for Psi4
+"""
+
 from psiresp.qm import QMEnergyOptions, QMGeometryOptimizationOptions
 from psiresp.conformer import Conformer, ConformerGenerationOptions
 from psiresp.orientation import Orientation
@@ -7,3 +12,11 @@ from psiresp.charge import ChargeConstraintOptions
 from psiresp.resp import RespOptions, RespCharges
 from psiresp.grid import GridOptions
 from psiresp.configs import *
+
+from ._version import get_versions
+
+# Handle versioneer
+versions = get_versions()
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
+del get_versions, versions
