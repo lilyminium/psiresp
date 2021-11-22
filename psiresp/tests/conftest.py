c0fe62fb9e1ff14cc5a3c5c9868b3eafef94238b
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2021-11-21 21:09:21 -0800

    return again

--- psiresp/tests/conftest.py
+++ psiresp/tests/conftest.py
@@ -21,26 +21,43 @@ pytest_plugins = [
 def postgres_server():
     storage = TemporaryPostgres(database_name="test_psiresp")
     storage.psql.restore_database(POSTGRES_SERVER_BACKUP)
-    yield storage.psql
+    return storage.psql
     storage.stop()
 
 
 @pytest.fixture(scope="session")
 def fractal_client(postgres_server):
-    with FractalSnowflake(
+    server = FractalSnowflake(
         max_workers=1,
         storage_project_name="test_psiresp",
         storage_uri=postgres_server.database_uri(),
         reset_database=False,
-        start_server=False,
-    ) as server:
-        return ptl.FractalClient(server)
+        start_server=False)
+    return ptl.FractalClient(server)
 
 
 @pytest.fixture(scope="function")
 def empty_client():
-    with FractalSnowflake() as server:
-        return ptl.FractalClient(server)
+    server = FractalSnowflake()
+    return ptl.FractalClient(server)
+
+
+# @pytest.fixture(scope="session")
+# def fractal_client(postgres_server):
+#     with FractalSnowflake(
+#         max_workers=1,
+#         storage_project_name="test_psiresp",
+#         storage_uri=postgres_server.database_uri(),
+#         reset_database=False,
+#         start_server=False,
+#     ) as server:
+#         yield ptl.FractalClient(server)
+
+
+# @pytest.fixture(scope="function")
+# def empty_client():
+#     with FractalSnowflake() as server:
+#         yield ptl.FractalClient(server)
 
 
 @pytest.fixture
